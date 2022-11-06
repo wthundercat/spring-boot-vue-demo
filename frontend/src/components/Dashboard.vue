0791206abf77b6d967fce374749a6606a4c863d2
wthundercat <waihein37@gmail.com>
2022-11-06 15:54:43 -0500

    Renamed file and added input and button

---
+++ frontend/src/components/Dashboard.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+<h1>{{ msg }}</h1>
+<div class="form-control">
+        <label>First Name</label>
+        <input type="text" v-model="firstName" name="firstName" placeholder="Enter fist Name" />
+      </div>
+      <div class="form-control">
+        <label>Last Name</label>
+        <input
+          type="text"
+          v-model="lastName"
+          name="age"
+          placeholder="Enter last name"
+        />
+      </div>
+      <input type="submit" value="Save Information" class="btn btn-block" />
+</div>
+
+
+
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+  data() {
+    return {
+      msg: ''
+    }
+  },
+  mounted() {
+    fetch("/api/hello")
+      .then((response) => response.text())
+      .then((data) => {
+          this.msg = data;
+      });
+  }
+}
+</script>
--- frontend/src/pages/Search.vue
+++ frontend/src/pages/Search.vue
@@ -1,4 +1,18 @@
 <template>
+<div class="form-control">
+      <label>Name</label>
+      <input type="text" v-model="firstName" name="firstName" placeholder="Enter Your Name" />
+    </div>
+    <div class="form-control">
+      <label>Age</label>
+      <input
+        type="text"
+        v-model="lastName"
+        name="age"
+        placeholder="Enter Your Age"
+      />
+    </div>
+    <input type="submit" value="Save Information" class="btn btn-block" />
 </template>
 
 <script>
